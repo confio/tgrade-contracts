dc961835b281156cd54e3dddc531f6a932c8182b
Mauro Lacy <mauro@lacy.com.es>
2021-04-20 13:48:51 +0200

    Add members() IndexedSnapshotMap

--- contracts/tg4-group/src/state.rs
+++ contracts/tg4-group/src/state.rs
@@ -1,6 +1,8 @@
 use cosmwasm_std::Addr;
 use cw_controllers::{Admin, Hooks};
-use cw_storage_plus::{Item, SnapshotMap, Strategy};
+use cw_storage_plus::{
+    Index, IndexList, IndexedSnapshotMap, Item, MultiIndex, PkOwned, SnapshotMap, Strategy, U64Key,
+};
 use tg4::TOTAL_KEY;
 
 pub const ADMIN: Admin = Admin::new("admin");
@@ -14,3 +16,32 @@ pub const MEMBERS: SnapshotMap<&Addr, u64> = SnapshotMap::new(
     tg4::MEMBERS_CHANGELOG,
     Strategy::EveryBlock,
 );
+
+pub struct MemberIndexes<'a> {
+    // pk goes to second tuple element
+    pub weight: MultiIndex<'a, (U64Key, PkOwned), u64>,
+}
+
+impl<'a> IndexList<u64> for MemberIndexes<'a> {
+    fn get_indexes(&'_ self) -> Box<dyn Iterator<Item = &'_ dyn Index<u64>> + '_> {
+        let v: Vec<&dyn Index<u64>> = vec![&self.weight];
+        Box::new(v.into_iter())
+    }
+}
+
+pub fn members<'a>() -> IndexedSnapshotMap<'a, &'a Addr, u64, MemberIndexes<'a>> {
+    let indexes = MemberIndexes {
+        weight: MultiIndex::new(
+            |&w, k| (U64Key::new(w), PkOwned(k)),
+            tg4::MEMBERS_KEY,
+            "members__weight",
+        ),
+    };
+    IndexedSnapshotMap::new(
+        tg4::MEMBERS_KEY,
+        tg4::MEMBERS_CHECKPOINTS,
+        tg4::MEMBERS_CHANGELOG,
+        Strategy::EveryBlock,
+        indexes,
+    )
+}
