35142e91f17bbc29df43dc67c350da8e1d247dc6
Mauro Lacy <mauro@lacy.com.es>
2021-09-02 13:15:13 +0200

    Add punish proposal, burn funds test

--- contracts/tgrade-dso/src/tests/unit_tests.rs
+++ contracts/tgrade-dso/src/tests/unit_tests.rs
@@ -922,7 +922,7 @@ fn leaving_voter_cannot_vote_anymore() {
 }
 
 #[test]
-fn propose_punish_members() {
+fn propose_punish_members_distribution() {
     let mut deps = mock_dependencies(&[]);
     let start = mock_env();
     let info = mock_info(INIT_ADMIN, &escrow_funds());
@@ -954,6 +954,9 @@ fn propose_punish_members() {
     let info = mock_info(VOTING1, &coins(ESCROW_FUNDS + 1, DENOM));
     execute_deposit_escrow(deps.as_mut(), later(&start, delay1 + 1), info).unwrap();
 
+    // Initial weights are proper
+    assert_voting(&deps, Some(1), Some(0), Some(0), Some(0), None);
+
     // Make a punish proposal
     let prop = ProposalContent::PunishMembers(vec![Punishment {
         member: VOTING1.into(),
@@ -965,7 +968,7 @@ fn propose_punish_members() {
     let msg = ExecuteMsg::Propose {
         title: "Punish VOTING1".to_string(),
         description:
-            "Punish VOTING1 with a 50% slashing. Distribute funds among VOTING2 and NONMEMBER"
+            "Punish VOTING1 with a 50% slashing. Distribute slashed funds among VOTING2 and NONMEMBER"
                 .to_string(),
         proposal: prop,
     };
@@ -1036,6 +1039,114 @@ fn propose_punish_members() {
         &res.messages[1],
         &SubMsg::new(BankMsg::Send {
             to_address: NONMEMBER.into(),
+            amount: vec![coin(ESCROW_FUNDS / 4, DSO_DENOM)]
+        })
+    );
+}
+
+#[test]
+fn propose_punish_members_burn() {
+    let mut deps = mock_dependencies(&[]);
+    let start = mock_env();
+    let info = mock_info(INIT_ADMIN, &escrow_funds());
+    do_instantiate(
+        deps.as_mut(),
+        info,
+        vec![VOTING1.into(), VOTING2.into(), VOTING3.into()],
+    )
+    .unwrap();
+
+    let voting_status = MemberStatus::Voting {};
+    let pending_status = MemberStatus::Pending {
+        proposal_id: PROPOSAL_ID_1,
+    };
+    let pending_status2 = MemberStatus::Pending {
+        proposal_id: PROPOSAL_ID_2,
+    };
+
+    // Add new members, and one of them pays in
+    let batch1 = vec![VOTING1.into(), VOTING2.into(), VOTING3.into()];
+    let delay1 = 10;
+    propose_add_voting_members_and_execute(
+        deps.as_mut(),
+        later(&start, delay1),
+        INIT_ADMIN,
+        batch1,
+    )
+    .unwrap();
+    let info = mock_info(VOTING1, &escrow_funds());
+    execute_deposit_escrow(deps.as_mut(), later(&start, delay1 + 1), info).unwrap();
+
+    // Initial weights are proper
+    assert_voting(&deps, Some(1), Some(0), Some(0), Some(0), None);
+
+    // Make a punish proposal
+    let prop = ProposalContent::PunishMembers(vec![Punishment {
+        member: VOTING1.into(),
+        slashing_percentage: Decimal::percent(25),
+        distribution_list: vec![],
+        burn_tokens: true,
+        kick_out: false,
+    }]);
+    let msg = ExecuteMsg::Propose {
+        title: "Punish VOTING1".to_string(),
+        description: "Punish VOTING1 with a 25% slashing. Burn slashed funds".to_string(),
+        proposal: prop,
+    };
+    let mut env = mock_env();
+    env.block.height += 10;
+    let res = execute(deps.as_mut(), env.clone(), mock_info(INIT_ADMIN, &[]), msg).unwrap();
+    let proposal_id = parse_prop_id(&res.attributes);
+
+    // ensure it passed (already via principal voter)
+    let prop = query_proposal(deps.as_ref(), env.clone(), proposal_id).unwrap();
+    assert_eq!(prop.status, Status::Passed);
+
+    // execute it
+    let res = execute(
+        deps.as_mut(),
+        env,
+        mock_info(NONVOTING1, &[]),
+        ExecuteMsg::Execute { proposal_id },
+    )
+    .unwrap();
+
+    // check the proper attributes returned
+    assert_eq!(res.attributes.len(), 8);
+    assert_eq!(&res.attributes[0], &attr("proposal", "punish_members"));
+    assert_eq!(&res.attributes[1], &attr("punishment", "1")); // First punishment in proposal
+    assert_eq!(&res.attributes[2], &attr("slashing", VOTING1));
+    assert_eq!(&res.attributes[3], &attr("slashing_percentage", "0.25"));
+    assert_eq!(&res.attributes[4], &attr("distribution_list", ""));
+    assert_eq!(&res.attributes[5], &attr("burn_tokens", "true"));
+    assert_eq!(&res.attributes[6], &attr("action", "execute"));
+    assert_eq!(&res.attributes[7], &attr("proposal_id", "2"));
+
+    // Check the escrow amounts, status and voting weight have been updated
+    // Weights properly
+    assert_voting(&deps, Some(1), Some(0), Some(0), Some(0), None);
+    // Check VOTING1 escrow is properly slashed
+    assert_escrow_paid(
+        &deps,
+        Some(ESCROW_FUNDS),
+        Some(ESCROW_FUNDS / 4 * 3),
+        Some(0),
+        Some(0),
+    );
+    // And status
+    assert_escrow_status(
+        &deps,
+        Some(voting_status),
+        Some(pending_status2),
+        Some(pending_status),
+        Some(pending_status),
+    );
+
+    // Assert the BankMsg is there
+    assert_eq!(res.messages.len(), 1);
+    assert_eq!(
+        &res.messages[0],
+        &SubMsg::new(BankMsg::Burn {
             amount: vec![coin(ESCROW_FUNDS / 4, DSO_DENOM)]
         })
     );
