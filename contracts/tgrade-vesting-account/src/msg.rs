f8f3a728db2077048e57fa145ab5928623ef1d58
Jakub Bogucki <jakub@confio.gmbh>
2021-09-23 22:25:09 +0200

    Change vesting accounts types to more consise VestingPlan

--- contracts/tgrade-vesting-account/src/msg.rs
+++ contracts/tgrade-vesting-account/src/msg.rs
@@ -1,10 +1,10 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use crate::state::VestingAccount;
+use crate::state::VestingPlan;
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
 #[serde(rename_all = "snake_case")]
 pub struct InstantiateMsg {
-    pub vesting_accounts: Vec<VestingAccount>,
+    pub vesting_accounts: Vec<VestingPlan>,
 }
--- contracts/tgrade-vesting-account/src/state.rs
+++ contracts/tgrade-vesting-account/src/state.rs
@@ -1,21 +1,22 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::Timestamp;
+use cosmwasm_std::{Timestamp, Uint128};
 
 /// If vesting account is discrete, tokens can't be transferred
 /// until given point of time.
 /// If account is continuous, then tokens will be released lineary
 /// starting at pre-defined point.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub enum VestingAccountType {
-    Discrete,
-    Continuous,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct VestingAccount {
-    pub tokens: u64,
-    pub release_at: Timestamp,
-    pub account_type: VestingAccountType,
+pub enum VestingPlan {
+    Discrete {
+        tokens: Uint128,
+        release_at: Timestamp,
+    },
+    Continuous {
+        tokens: Uint128,
+        start_at: Timestamp,
+        /// end_at allows linear interpolation between these points.
+        end_at: Timestamp,
+    },
 }
