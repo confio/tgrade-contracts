be1efa12e45b307d27ccb7fc6b63fe0bcbccf99a
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-10-18 18:21:49 +0200

    Fix linter warnings

--- packages/bindings/src/lib.rs
+++ packages/bindings/src/lib.rs
@@ -18,4 +18,4 @@ pub use validator::{
 // This is a signal, such that any contract that imports these helpers will only run on the tgrade
 // blockchain
 #[no_mangle]
-extern "C" fn requires_tgrade() -> () {}
+extern "C" fn requires_tgrade() {}
