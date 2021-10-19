1deaa45e65c4b3a52a71ce87721650a0038aefcd
Bartłomiej Kuras <bart.k@confio.gmbh>
2021-10-19 10:40:48 +0200

    tgrade-trusted-circle: Test suite builder for deny list testing

--- contracts/tgrade-trusted-circle/src/tests/deny_list.rs
+++ contracts/tgrade-trusted-circle/src/tests/deny_list.rs
@@ -1,8 +1,10 @@
-use cosmwasm_std::Addr;
-use cw_multi_test::{Contract, ContractWrapper};
+use cosmwasm_std::{Addr, Decimal, Uint128};
+use cw_multi_test::{Contract, ContractWrapper, Executor};
 use derivative::Derivative;
 use tg_bindings::TgradeMsg;
 use tg_bindings_test::TgradeApp;
+
+use crate::msg::InstantiateMsg;
 
 fn contract_trusted_circle() -> Box<dyn Contract<TgradeMsg>> {
     Box::new(ContractWrapper::new(
@@ -29,13 +31,65 @@ struct SuiteBuilder {
 }
 
 impl SuiteBuilder {
-    fn with_denied(mut self, addr: &str) -> Self {
+    pub fn with_denied(mut self, addr: &str) -> Self {
         self.deny_list.push(addr.to_owned());
         self
     }
 
-    fn with_member(mut self, addr: &str) -> Self {
+    pub fn with_member(mut self, addr: &str) -> Self {
         self.members.push(addr.to_owned());
         self
+    }
+
+    pub fn build(self) -> Suite {
+        let owner = Addr::unchecked("owner");
+        let mut app = TgradeApp::new(owner.as_str());
+
+        let contract_id = app.store_code(contract_trusted_circle());
+        let deny_list = app
+            .instantiate_contract(
+                contract_id,
+                owner.clone(),
+                &InstantiateMsg {
+                    name: "Deny list".to_owned(),
+                    escrow_amount: Uint128::zero(),
+                    voting_period: 1,
+                    quorum: Decimal::percent(50),
+                    threshold: Decimal::zero(),
+                    allow_end_early: true,
+                    initial_members: self.deny_list,
+                    deny_list: None,
+                },
+                &[],
+                "deny-list",
+                Some(owner.to_string()),
+            )
+            .unwrap();
+
+        let contract = app
+            .instantiate_contract(
+                contract_id,
+                owner.clone(),
+                &InstantiateMsg {
+                    name: "Trusted Circle".to_owned(),
+                    escrow_amount: Uint128::zero(),
+                    voting_period: 1,
+                    quorum: Decimal::percent(50),
+                    threshold: Decimal::zero(),
+                    allow_end_early: true,
+                    initial_members: self.members,
+                    deny_list: Some(deny_list.to_string()),
+                },
+                &[],
+                "trusted-circle",
+                Some(owner.to_string()),
+            )
+            .unwrap();
+
+        Suite {
+            app,
+            deny_list,
+            contract,
+        }
     }
 }
