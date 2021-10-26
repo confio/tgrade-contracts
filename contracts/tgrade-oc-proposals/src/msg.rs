6564b2d66b7b0a8814ac252a53ee37de931a4a1d
Jakub Bogucki <jakub@confio.gmbh>
2021-10-26 11:18:48 +0200

    Add enum OversightProposal in msg

--- contracts/tgrade-oc-proposals/src/msg.rs
+++ contracts/tgrade-oc-proposals/src/msg.rs
@@ -36,6 +36,14 @@ pub enum ExecuteMsg {
     MemberChangedHook(MemberChangedHookMsg),
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum OversightProposal {
+    DistributeEngagementRewards(Vec<String>),
+    JailValidator(String),
+    UnjailValidator(String),
+}
+
 // We can also add this as a cw3 extension
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
 #[serde(rename_all = "snake_case")]
