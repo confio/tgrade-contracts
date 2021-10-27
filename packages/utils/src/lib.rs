be195f08de7c97facf1044986bc0abe604843df1
Tomasz Kurcz <uint@lavabit.com>
2021-10-27 11:00:35 +0200

    Add slashers store to tg-utils

--- packages/utils/src/lib.rs
+++ packages/utils/src/lib.rs
@@ -1,9 +1,11 @@
 mod hooks;
 mod member_indexes;
 mod preauth;
+mod slashers;
 mod time;
 
 pub use hooks::{HookError, Hooks};
-pub use member_indexes::{members, ADMIN, HOOKS, PREAUTH, TOTAL};
+pub use member_indexes::{members, ADMIN, HOOKS, PREAUTH, PREAUTH_SLASHING, SLASHERS, TOTAL};
 pub use preauth::{Preauth, PreauthError};
+pub use slashers::{SlasherError, Slashers};
 pub use time::{Duration, Expiration};
