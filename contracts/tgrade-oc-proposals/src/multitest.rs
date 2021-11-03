404cc7237bcc9c572959292a337a3944e7d527d5
Jakub Bogucki <jakub@confio.gmbh>
2021-11-03 15:33:23 +0100

    Move test close works into multitest

--- contracts/tgrade-oc-proposals/src/multitest.rs
+++ contracts/tgrade-oc-proposals/src/multitest.rs
@@ -58,17 +58,17 @@ fn grant_engagement_reward() {
     let members = vec!["owner", "voter1", "voter2", "voter3"];
 
     let mut suite = SuiteBuilder::new()
-        .with_group_member(members[0], 0)
-        .with_group_member(members[1], 1)
-        .with_group_member(members[2], 2)
-        .with_group_member(members[3], 3)
+        .with_group_member(members[0], 1)
+        .with_group_member(members[1], 2)
+        .with_group_member(members[2], 3)
+        .with_group_member(members[3], 4)
         .with_engagement_member(members[1], 0)
         .with_voting_rules(mock_rules().threshold(Decimal::percent(50)).build())
         .with_multisig_as_group_admin(true)
         .build();
 
     // Proposal granting 10 engagement points to voter1
-    // Proposing member has 0 voting power
+    // Proposing member has 1 voting power
     let response = suite
         .propose_grant_engagement(members[0], members[1], 10)
         .unwrap();
@@ -121,10 +121,10 @@ fn execute_group_can_change() {
     let members = vec!["owner", "voter1", "voter2", "voter3"];
 
     let mut suite = SuiteBuilder::new()
-        .with_group_member(members[0], 0)
-        .with_group_member(members[1], 1)
-        .with_group_member(members[2], 2)
-        .with_group_member(members[3], 3)
+        .with_group_member(members[0], 1)
+        .with_group_member(members[1], 2)
+        .with_group_member(members[2], 3)
+        .with_group_member(members[3], 4)
         .with_voting_rules(mock_rules().threshold(Decimal::percent(51)).build())
         .build();
 
@@ -187,4 +187,45 @@ fn execute_group_can_change() {
     suite.vote(members[3], proposal_id, Vote::Yes).unwrap();
     let proposal_status = suite.query_proposal_status(proposal_id).unwrap();
     assert_eq!(proposal_status, Status::Passed);
+}
+
+#[test]
+fn close_proposal() {
+    let members = vec!["owner", "voter1", "voter2", "voter3"];
+
+    let rules = mock_rules().threshold(Decimal::percent(51)).build();
+
+    let mut suite = SuiteBuilder::new()
+        .with_group_member(members[0], 1)
+        .with_group_member(members[1], 2)
+        .with_voting_rules(rules.clone())
+        .build();
+
+    // Create proposal with 1 voting power
+    let response = suite
+        .propose_grant_engagement(members[0], members[1], 10)
+        .unwrap();
+    let proposal_id: u64 = get_proposal_id(&response).unwrap();
+
+    // Non-expired proposals cannot be closed
+    let err = suite.close("anybody", proposal_id).unwrap_err();
+    assert_eq!(ContractError::NotExpired {}, err.downcast().unwrap());
+
+    // Move time forward so proposal expires
+    suite.app.advance_seconds(rules.voting_period_secs());
+
+    // Expired proposals can be closed
+    let response = suite.close("anybody", proposal_id).unwrap();
+    assert_eq!(
+        response.custom_attrs(1),
+        [
+            ("action", "close"),
+            ("sender", "anybody"),
+            ("proposal_id", proposal_id.to_string().as_str()),
+        ],
+    );
+
+    // Closing second time causes error
+    let err = suite.close("anybody", proposal_id).unwrap_err();
+    assert_eq!(ContractError::WrongCloseStatus {}, err.downcast().unwrap());
 }
