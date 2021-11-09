bdb8066157c87b0bf5a01d4f36d922fb3385c010
Bartłomiej Kuras <bart.k@confio.gmbh>
2021-11-09 12:05:18 +0100

    tg-validator-voting: Contract created

---
+++ contracts/tgrade-validator-voting/src/msg.rs
@@ -0,0 +1,71 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cw3::Vote;
+
+use crate::state::{OversightProposal, VotingRules};
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct InstantiateMsg {
+    pub rules: VotingRules,
+    // this is the group contract that contains the member list
+    pub group_addr: String,
+    // this is the engagement contract that contains list for engagement rewards
+    pub engagement_addr: String,
+}
+
+// TODO: add some T variants? Maybe good enough as fixed Empty for now
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    Propose {
+        title: String,
+        description: String,
+        proposal: OversightProposal,
+    },
+    Vote {
+        proposal_id: u64,
+        vote: Vote,
+    },
+    Execute {
+        proposal_id: u64,
+    },
+    Close {
+        proposal_id: u64,
+    },
+}
+
+// We can also add this as a cw3 extension
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    /// Return VotingRules
+    Rules {},
+    /// Returns ProposalResponse
+    Proposal { proposal_id: u64 },
+    /// Returns ProposalListResponse
+    ListProposals {
+        start_after: Option<u64>,
+        limit: Option<u32>,
+    },
+    /// Returns ProposalListResponse
+    ReverseProposals {
+        start_before: Option<u64>,
+        limit: Option<u32>,
+    },
+    /// Returns VoteResponse
+    Vote { proposal_id: u64, voter: String },
+    /// Returns VoteListResponse
+    ListVotes {
+        proposal_id: u64,
+        start_after: Option<String>,
+        limit: Option<u32>,
+    },
+    /// Returns VoterResponse
+    Voter { address: String },
+    /// Returns VoterListResponse
+    ListVoters {
+        start_after: Option<String>,
+        limit: Option<u32>,
+    },
+}
