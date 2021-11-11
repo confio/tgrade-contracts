ed6161f649f5da88fbc621bda493c09a085c9e92
Tomasz Kurcz <uint@lavabit.com>
2021-11-11 15:08:51 +0100

    tgrade-valset: add UpdateAdmin tests

---
+++ contracts/tgrade-valset/src/multitest/admin.rs
@@ -0,0 +1,48 @@
+use crate::error::ContractError;
+
+use super::helpers::members_init;
+use super::suite::SuiteBuilder;
+use anyhow::Result as AnyResult;
+use cw_controllers::AdminError;
+use cw_multi_test::AppResponse;
+
+fn assert_success(resp: AnyResult<AppResponse>) {
+    assert!(resp.is_ok());
+}
+
+fn assert_unauthorized(resp: AnyResult<AppResponse>) {
+    const NOT_ADMIN_ERR: ContractError = ContractError::AdminError(AdminError::NotAdmin {});
+
+    assert_eq!(NOT_ADMIN_ERR, resp.unwrap_err().downcast().unwrap());
+}
+
+#[test]
+fn admin_can_change_admin() {
+    let mut suite = SuiteBuilder::new().build();
+
+    let admin = suite.admin().to_string();
+
+    assert_success(suite.update_admin(&admin, "asd".to_string()));
+}
+
+#[test]
+fn non_admin_cannot_change_admin() {
+    let members = vec!["member1", "member2", "member3", "member4"];
+
+    let mut suite = SuiteBuilder::new()
+        .with_operators(&members_init(&members, &[2, 3, 5, 8]), &[])
+        .build();
+
+    assert_unauthorized(suite.update_admin("random guy", "asd".to_string()));
+    assert_unauthorized(suite.update_admin(members[0], "asd".to_string()));
+}
+
+#[test]
+fn admin_cannot_update_admin_twice() {
+    let mut suite = SuiteBuilder::new().build();
+
+    let admin = suite.admin().to_string();
+
+    assert_success(suite.update_admin(&admin, "asd".to_string()));
+    assert_unauthorized(suite.update_admin(&admin, "asd".to_string()));
+}
