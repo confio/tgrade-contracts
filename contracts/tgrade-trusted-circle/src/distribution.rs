468b7a2a4dc19aab19b0564ee82765e4b801ee8f
Bartłomiej Kuras <bart.k@confio.gmbh>
2022-02-01 18:30:58 +0100

    tgrade-trusted-circle: Cw2222 distribution mechanizm UTs

--- contracts/tgrade-trusted-circle/src/distribution.rs
+++ contracts/tgrade-trusted-circle/src/distribution.rs
@@ -35,8 +35,6 @@ pub struct WithdrawAdjustment {
     pub points_correction: Int128,
     /// How much funds addresses already withdrawn.
     pub withdrawn_funds: Uint128,
-    /// User delegated for funds withdrawal
-    pub delegated: Addr,
 }
 
 /// Tokens distribution data
@@ -100,7 +98,12 @@ pub fn distribute_funds(deps: DepsMut, env: Env, total: u128) -> Result<Coin, Co
 /// Returns Coin which should be send to receiver as a withdrawal
 pub fn withdraw_tokens(deps: DepsMut, owner: &Addr, weight: u128) -> Result<Coin, ContractError> {
     let mut distribution = DISTRIBUTION.load(deps.storage)?;
-    let mut adjustment = WITHDRAW_ADJUSTMENT.load(deps.storage, owner)?;
+    let mut adjustment = WITHDRAW_ADJUSTMENT
+        .may_load(deps.storage, owner)?
+        .unwrap_or_else(|| WithdrawAdjustment {
+            points_correction: Int128::zero(),
+            withdrawn_funds: Uint128::zero(),
+        });
 
     let token = withdrawable_funds(weight, &distribution, &adjustment)?;
     if token.amount.is_zero() {
@@ -135,11 +138,12 @@ pub fn withdrawable_funds(
 
 pub fn adjusted_withdrawable_funds(deps: Deps, owner: Addr, weight: u128) -> StdResult<Coin> {
     let distribution = DISTRIBUTION.load(deps.storage)?;
-    let adjustment = if let Some(adj) = WITHDRAW_ADJUSTMENT.may_load(deps.storage, &owner)? {
-        adj
-    } else {
-        return Ok(coin(0, distribution.denom));
-    };
+    let adjustment = WITHDRAW_ADJUSTMENT
+        .may_load(deps.storage, &owner)?
+        .unwrap_or_else(|| WithdrawAdjustment {
+            points_correction: 0.into(),
+            withdrawn_funds: Uint128::zero(),
+        });
 
     let token = withdrawable_funds(weight, &distribution, &adjustment)?;
     Ok(token)
@@ -183,7 +187,6 @@ pub fn apply_points_correction(
             WithdrawAdjustment {
                 points_correction: 0.into(),
                 withdrawn_funds: Uint128::zero(),
-                delegated: addr.clone(),
             }
         });
         let points_correction: i128 = old.points_correction.into();
@@ -191,4 +194,447 @@ pub fn apply_points_correction(
         Ok(old)
     })?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, MOCK_CONTRACT_ADDR};
+    use cosmwasm_std::{coin, coins, Addr};
+
+    const DENOM: &str = "utgd";
+
+    struct Member {
+        addr: Addr,
+        weight: u128,
+    }
+
+    #[test]
+    fn initialization() {
+        let mut deps = mock_dependencies();
+        init_distribution(deps.as_mut(), DENOM.to_owned()).unwrap();
+
+        let distributed = distributed_funds(deps.as_ref()).unwrap();
+        assert_eq!(distributed, coin(0, DENOM));
+
+        let undistributed = undistributed_funds(deps.as_ref(), mock_env()).unwrap();
+        assert_eq!(undistributed, coin(0, DENOM));
+    }
+
+    #[test]
+    fn divisible_funds_distributed() {
+        let mut deps = mock_dependencies();
+        init_distribution(deps.as_mut(), DENOM.to_owned()).unwrap();
+
+        let members = vec![
+            Member {
+                addr: Addr::unchecked("member0"),
+                weight: 1,
+            },
+            Member {
+                addr: Addr::unchecked("member1"),
+                weight: 3,
+            },
+        ];
+        let total_weight = 4;
+
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(1000, DENOM));
+        let distributed = distribute_funds(deps.as_mut(), mock_env(), total_weight).unwrap();
+        assert_eq!(distributed, coin(1000, DENOM));
+
+        let funds =
+            adjusted_withdrawable_funds(deps.as_ref(), members[0].addr.clone(), members[0].weight)
+                .unwrap();
+        assert_eq!(funds, coin(250, DENOM));
+
+        let funds =
+            adjusted_withdrawable_funds(deps.as_ref(), members[1].addr.clone(), members[1].weight)
+                .unwrap();
+        assert_eq!(funds, coin(750, DENOM));
+
+        let distributed = distributed_funds(deps.as_ref()).unwrap();
+        assert_eq!(distributed, coin(1000, DENOM));
+
+        let undistributed = undistributed_funds(deps.as_ref(), mock_env()).unwrap();
+        assert_eq!(undistributed, coin(0, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[0].addr, members[0].weight).unwrap();
+        assert_eq!(funds, coin(250, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(750, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[1].addr, members[1].weight).unwrap();
+        assert_eq!(funds, coin(750, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(0, DENOM));
+
+        let funds =
+            adjusted_withdrawable_funds(deps.as_ref(), members[0].addr.clone(), members[0].weight)
+                .unwrap();
+        assert_eq!(funds, coin(0, DENOM));
+
+        let funds =
+            adjusted_withdrawable_funds(deps.as_ref(), members[1].addr.clone(), members[1].weight)
+                .unwrap();
+        assert_eq!(funds, coin(0, DENOM));
+
+        let distributed = distributed_funds(deps.as_ref()).unwrap();
+        assert_eq!(distributed, coin(1000, DENOM));
+
+        let undistributed = undistributed_funds(deps.as_ref(), mock_env()).unwrap();
+        assert_eq!(undistributed, coin(0, DENOM));
+    }
+
+    #[test]
+    fn divisible_funds_distributed_twice() {
+        let mut deps = mock_dependencies();
+        init_distribution(deps.as_mut(), DENOM.to_owned()).unwrap();
+
+        let members = vec![
+            Member {
+                addr: Addr::unchecked("member0"),
+                weight: 1,
+            },
+            Member {
+                addr: Addr::unchecked("member1"),
+                weight: 3,
+            },
+        ];
+        let total_weight = 4;
+
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(1000, DENOM));
+        let distributed = distribute_funds(deps.as_mut(), mock_env(), total_weight).unwrap();
+        assert_eq!(distributed, coin(1000, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[0].addr, members[0].weight).unwrap();
+        assert_eq!(funds, coin(250, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(750, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[1].addr, members[1].weight).unwrap();
+        assert_eq!(funds, coin(750, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(0, DENOM));
+
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(500, DENOM));
+        let distributed = distribute_funds(deps.as_mut(), mock_env(), total_weight).unwrap();
+        assert_eq!(distributed, coin(500, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[0].addr, members[0].weight).unwrap();
+        assert_eq!(funds, coin(125, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(375, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[1].addr, members[1].weight).unwrap();
+        assert_eq!(funds, coin(375, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(0, DENOM));
+
+        let distributed = distributed_funds(deps.as_ref()).unwrap();
+        assert_eq!(distributed, coin(1500, DENOM));
+
+        let undistributed = undistributed_funds(deps.as_ref(), mock_env()).unwrap();
+        assert_eq!(undistributed, coin(0, DENOM));
+    }
+
+    #[test]
+    fn divisible_funds_distributed_twice_accumulated() {
+        let mut deps = mock_dependencies();
+        init_distribution(deps.as_mut(), DENOM.to_owned()).unwrap();
+
+        let members = vec![
+            Member {
+                addr: Addr::unchecked("member0"),
+                weight: 1,
+            },
+            Member {
+                addr: Addr::unchecked("member1"),
+                weight: 3,
+            },
+        ];
+        let total_weight = 4;
+
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(1000, DENOM));
+        let distributed = distribute_funds(deps.as_mut(), mock_env(), total_weight).unwrap();
+        assert_eq!(distributed, coin(1000, DENOM));
+
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(1500, DENOM));
+        let distributed = distribute_funds(deps.as_mut(), mock_env(), total_weight).unwrap();
+        assert_eq!(distributed, coin(500, DENOM));
+
+        let funds =
+            adjusted_withdrawable_funds(deps.as_ref(), members[0].addr.clone(), members[0].weight)
+                .unwrap();
+        assert_eq!(funds, coin(375, DENOM));
+
+        let funds =
+            adjusted_withdrawable_funds(deps.as_ref(), members[1].addr.clone(), members[1].weight)
+                .unwrap();
+        assert_eq!(funds, coin(1125, DENOM));
+
+        let distributed = distributed_funds(deps.as_ref()).unwrap();
+        assert_eq!(distributed, coin(1500, DENOM));
+
+        let undistributed = undistributed_funds(deps.as_ref(), mock_env()).unwrap();
+        assert_eq!(undistributed, coin(0, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[0].addr, members[0].weight).unwrap();
+        assert_eq!(funds, coin(375, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(1125, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[1].addr, members[1].weight).unwrap();
+        assert_eq!(funds, coin(1125, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(0, DENOM));
+
+        let funds =
+            adjusted_withdrawable_funds(deps.as_ref(), members[0].addr.clone(), members[0].weight)
+                .unwrap();
+        assert_eq!(funds, coin(0, DENOM));
+
+        let funds =
+            adjusted_withdrawable_funds(deps.as_ref(), members[1].addr.clone(), members[1].weight)
+                .unwrap();
+        assert_eq!(funds, coin(0, DENOM));
+
+        let distributed = distributed_funds(deps.as_ref()).unwrap();
+        assert_eq!(distributed, coin(1500, DENOM));
+
+        let undistributed = undistributed_funds(deps.as_ref(), mock_env()).unwrap();
+        assert_eq!(undistributed, coin(0, DENOM));
+    }
+
+    #[test]
+    fn distribution_with_leftover() {
+        let mut deps = mock_dependencies();
+        init_distribution(deps.as_mut(), DENOM.to_owned()).unwrap();
+
+        let members = vec![
+            Member {
+                addr: Addr::unchecked("member0"),
+                weight: 7,
+            },
+            Member {
+                addr: Addr::unchecked("member1"),
+                weight: 11,
+            },
+            Member {
+                addr: Addr::unchecked("member2"),
+                weight: 13,
+            },
+        ];
+        let total_weight = 31;
+
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(100, DENOM));
+        let distributed = distribute_funds(deps.as_mut(), mock_env(), total_weight).unwrap();
+        assert_eq!(distributed, coin(100, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[0].addr, members[0].weight).unwrap();
+        assert_eq!(funds, coin(22, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(78, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[1].addr, members[1].weight).unwrap();
+        assert_eq!(funds, coin(35, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(43, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[2].addr, members[2].weight).unwrap();
+        assert_eq!(funds, coin(41, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(2, DENOM));
+
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(3002, DENOM));
+        let distributed = distribute_funds(deps.as_mut(), mock_env(), total_weight).unwrap();
+        assert_eq!(distributed, coin(3000, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[0].addr, members[0].weight).unwrap();
+        assert_eq!(funds, coin(678, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(2324, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[1].addr, members[1].weight).unwrap();
+        assert_eq!(funds, coin(1065, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(1259, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[2].addr, members[2].weight).unwrap();
+        assert_eq!(funds, coin(1259, DENOM));
+    }
+
+    #[test]
+    fn distribution_with_leftover_accumulated() {
+        let mut deps = mock_dependencies();
+        init_distribution(deps.as_mut(), DENOM.to_owned()).unwrap();
+
+        let members = vec![
+            Member {
+                addr: Addr::unchecked("member0"),
+                weight: 7,
+            },
+            Member {
+                addr: Addr::unchecked("member1"),
+                weight: 11,
+            },
+            Member {
+                addr: Addr::unchecked("member2"),
+                weight: 13,
+            },
+        ];
+        let total_weight = 31;
+
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(100, DENOM));
+        let distributed = distribute_funds(deps.as_mut(), mock_env(), total_weight).unwrap();
+        assert_eq!(distributed, coin(100, DENOM));
+
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(3100, DENOM));
+        let distributed = distribute_funds(deps.as_mut(), mock_env(), total_weight).unwrap();
+        assert_eq!(distributed, coin(3000, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[0].addr, members[0].weight).unwrap();
+        assert_eq!(funds, coin(700, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(2300, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[1].addr, members[1].weight).unwrap();
+        assert_eq!(funds, coin(1100, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(1300, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[2].addr, members[2].weight).unwrap();
+        assert_eq!(funds, coin(1300, DENOM));
+    }
+
+    #[test]
+    fn weight_changed_after_distribution() {
+        let mut deps = mock_dependencies();
+        init_distribution(deps.as_mut(), DENOM.to_owned()).unwrap();
+
+        let mut members = vec![
+            Member {
+                addr: Addr::unchecked("member0"),
+                weight: 1,
+            },
+            Member {
+                addr: Addr::unchecked("member1"),
+                weight: 2,
+            },
+            Member {
+                addr: Addr::unchecked("member2"),
+                weight: 5,
+            },
+        ];
+        let mut total_weight = 8;
+
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(400, DENOM));
+        let distributed = distribute_funds(deps.as_mut(), mock_env(), total_weight).unwrap();
+        assert_eq!(distributed, coin(400, DENOM));
+
+        members[0].weight = 6;
+        members[1].weight = 0;
+        members[2].weight = 5;
+        total_weight = 11;
+
+        let ppw = points_per_weight(deps.as_ref()).unwrap();
+        apply_points_correction(deps.as_mut(), &members[0].addr, ppw, 5).unwrap();
+        apply_points_correction(deps.as_mut(), &members[1].addr, ppw, -2).unwrap();
+        apply_points_correction(deps.as_mut(), &members[2].addr, ppw, 0).unwrap();
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[0].addr, members[0].weight).unwrap();
+        assert_eq!(funds, coin(50, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(350, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[1].addr, members[1].weight).unwrap();
+        assert_eq!(funds, coin(100, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(250, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[2].addr, members[2].weight).unwrap();
+        assert_eq!(funds, coin(250, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(0, DENOM));
+
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(1100, DENOM));
+        let distributed = distribute_funds(deps.as_mut(), mock_env(), total_weight).unwrap();
+        assert_eq!(distributed, coin(1100, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[0].addr, members[0].weight).unwrap();
+        assert_eq!(funds, coin(600, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(500, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[1].addr, members[1].weight).unwrap();
+        assert_eq!(funds, coin(0, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[2].addr, members[2].weight).unwrap();
+        assert_eq!(funds, coin(500, DENOM));
+    }
+
+    #[test]
+    fn weight_changed_after_distribution_accumulated() {
+        let mut deps = mock_dependencies();
+        init_distribution(deps.as_mut(), DENOM.to_owned()).unwrap();
+
+        let mut members = vec![
+            Member {
+                addr: Addr::unchecked("member0"),
+                weight: 1,
+            },
+            Member {
+                addr: Addr::unchecked("member1"),
+                weight: 2,
+            },
+            Member {
+                addr: Addr::unchecked("member2"),
+                weight: 5,
+            },
+        ];
+        let mut total_weight = 8;
+
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(400, DENOM));
+        let distributed = distribute_funds(deps.as_mut(), mock_env(), total_weight).unwrap();
+        assert_eq!(distributed, coin(400, DENOM));
+
+        members[0].weight = 6;
+        members[1].weight = 0;
+        members[2].weight = 5;
+        total_weight = 11;
+
+        let ppw = points_per_weight(deps.as_ref()).unwrap();
+        apply_points_correction(deps.as_mut(), &members[0].addr, ppw, 5).unwrap();
+        apply_points_correction(deps.as_mut(), &members[1].addr, ppw, -2).unwrap();
+        apply_points_correction(deps.as_mut(), &members[2].addr, ppw, 0).unwrap();
+
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(1500, DENOM));
+        let distributed = distribute_funds(deps.as_mut(), mock_env(), total_weight).unwrap();
+        assert_eq!(distributed, coin(1100, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[0].addr, members[0].weight).unwrap();
+        assert_eq!(funds, coin(650, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(850, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[1].addr, members[1].weight).unwrap();
+        assert_eq!(funds, coin(100, DENOM));
+        deps.querier
+            .update_balance(MOCK_CONTRACT_ADDR, coins(750, DENOM));
+
+        let funds = withdraw_tokens(deps.as_mut(), &members[2].addr, members[2].weight).unwrap();
+        assert_eq!(funds, coin(750, DENOM));
+    }
 }
