7362fe09a4caa2ddc5ce5d87928dab2cc657683d
Mauro Lacy <mauro@confio.gmbh>
2022-05-09 18:12:42 +0200

    Remove unused messages / comments

--- contracts/tgrade-tc-payments/src/msg.rs
+++ contracts/tgrade-tc-payments/src/msg.rs
@@ -35,17 +35,6 @@ impl Period {
     }
 }
 
-#[allow(clippy::large_enum_variant)]
-#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
-#[serde(rename_all = "snake_case")]
-pub enum ExecuteMsg {}
-
-// TODO: expose batch query
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {}
-
-// #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
-// pub struct RulesResponse {
-//     pub rules: VotingRules,
-// }
