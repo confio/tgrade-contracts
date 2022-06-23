60469e11b7f24f463b4f2d8decec4197d9b375fb
Mauro Lacy <mauro@confio.gmbh>
2022-06-23 12:42:53 +0200

    Add ArbiterPoolProposal to schema

--- contracts/tgrade-ap-voting/examples/schema.rs
+++ contracts/tgrade-ap-voting/examples/schema.rs
@@ -2,11 +2,11 @@ use std::env::current_dir;
 use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, export_schema_with_title, remove_schemas, schema_for};
-use cosmwasm_std::Empty as EmptyProposal;
 
 use tg_voting_contract::msg::ProposalCreationResponse;
 use tg_voting_contract::state::{ProposalListResponse, ProposalResponse};
 use tgrade_ap_voting::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use tgrade_ap_voting::state::ArbiterPoolProposal;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -17,8 +17,14 @@ fn main() {
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
     export_schema_with_title(&schema_for!(ExecuteMsg), &out_dir, "ExecuteMsg");
     export_schema_with_title(&schema_for!(QueryMsg), &out_dir, "QueryMsg");
-    export_schema(&schema_for!(EmptyProposal), &out_dir);
-    export_schema(&schema_for!(ProposalResponse<EmptyProposal>), &out_dir);
+    export_schema(&schema_for!(ArbiterPoolProposal), &out_dir);
+    export_schema(
+        &schema_for!(ProposalResponse<ArbiterPoolProposal>),
+        &out_dir,
+    );
     export_schema(&schema_for!(ProposalCreationResponse), &out_dir);
-    export_schema(&schema_for!(ProposalListResponse<EmptyProposal>), &out_dir);
+    export_schema(
+        &schema_for!(ProposalListResponse<ArbiterPoolProposal>),
+        &out_dir,
+    );
 }
